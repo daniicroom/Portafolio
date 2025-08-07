5f496da475b967170325f527a01102793c07579c
jvalbuenaTreda <jvalbuena@tredasolutions.com>
2025-08-07 15:23:08 -0500

    update names, and delete images

--- public/img/logos/index.ts
+++ public/img/logos/index.ts
@@ -1,6 +1,6 @@
 import TredaLogo from "./treda.png";
 import NoLogo from "./no-logo.png";
-import AgRisk from "./agrisk.png";
+import AgRisk from "./ag-risk.png";
 import FirmaSeguro from "./fs.png";
 import AuraQuantic from "./auraquantic.png";
 
